6c143d6832f68223df54d344dabf96c71380c4d3
Louis Heal <louisheal@live.co.uk>
2024-12-03 22:36:50 +0000

    move channel tests to correct file

--- test/repositories/channel_test.go
+++ test/repositories/channel_test.go
@@ -1,1 +1,48 @@
 package repositories_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/streampets/backend/models"
+	"github.com/streampets/backend/repositories"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupTestDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.AutoMigrate(&models.Channel{}); err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func TestGetOverlayID(t *testing.T) {
+	db := setupTestDB()
+	repo := repositories.NewChannelRepository(db)
+
+	channelID := models.UserID("channel id")
+	channelName := "channel name"
+	expectedID := uuid.New()
+
+	channel := models.Channel{ChannelID: channelID, ChannelName: channelName, OverlayID: expectedID}
+
+	if err := db.Create(&channel).Error; err != nil {
+		t.Errorf("could not create record")
+	}
+
+	overlayID, err := repo.GetOverlayID(channelID)
+	if err != nil {
+		t.Errorf("did not expect an error but received: %s", err.Error())
+	}
+
+	if overlayID != expectedID {
+		t.Errorf("expected %s got %s", expectedID, overlayID)
+	}
+}
